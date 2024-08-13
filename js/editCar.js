f5d238bb46914f0e240d5ca4dca32572f2379c68
atheersnm@gmail.com <atheersnm@gmail.com>
2024-08-13 20:37:41 +0300

    function for edit car by id

---
+++ js/editCar.js
@@ -0,0 +1,25 @@
+export function editCar(event) {
+    const carId = event.target.getAttribute('data-id');
+    fetch(`http://localhost:3000/cars/get`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id: carId }),
+    })
+    .then(response => response.json())
+    .then(car => {
+        document.getElementById('car-name').value = car.name;
+        document.getElementById('car-type').value = car.type;
+        document.getElementById('car-color').value = car.color;
+        document.getElementById('car-feedback').value = car.feedback;
+
+        // Change button icon to edit
+        const submitButtonIcon = document.querySelector('.add-icon i');
+        submitButtonIcon.classList.remove('fa-plus');
+        submitButtonIcon.classList.add('fa-edit');
+
+        window.editingCarId = carId;  // Global variable to track the editing car ID
+    })
+    .catch(error => console.error('Error fetching car data:', error));
+}
